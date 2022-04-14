05c06619cd7a3b8f2b171b367768d192d7324dd8
alishtu <alishertuleu0@gmail.com>
2022-04-14 19:36:29 +0600

    'demo'

---
+++ demo/src/main/java/com/example/demo/controller.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+
+public class controller {
+    @GetMapping("/hi")
+    public String hi(){
+        return "Hi ! ! ! ";
+    }
+    @GetMapping("/bye")
+public String bye(){
+        return "Bye ! ! ! !";
+    }
+}
